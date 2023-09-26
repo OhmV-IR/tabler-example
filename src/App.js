8f4cf789cefb7470c6c1d598fb7ceb2e1cc2eea0
AB <bourdeaux.adrien@gmail.com>
2023-09-26 11:40:06 -0400

    added an if/else statement in html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import './App.css';
-import {IconBrandPinterest, IconSettings, IconPower, IconLock, IconCheck, IconX} from '@tabler/icons-react'
+import React, {useState} from 'react';
+import {IconBrandPinterest, IconCloudLockOpen, IconSettings, IconPower, IconLock, IconCheck, IconBrandTwitter} from '@tabler/icons-react'
 function App() {
+  // adds a locked variable and creates a function called setLocked
+  const [locked, setLocked] = useState(false)
+  const triggerLock = async event => {
+    //code to lock here
+    await setLocked(true);
+  }
+  const triggerUnlock = async event => {
+    // code to unlock here
+    await setLocked(false);
+  }
+  // {locked
+  // ? yes condition html goes here
+  // : no condition html goes here}
   return (
     <div className = "leftAlign">
-    <p>Using tabler, you can add icons to represent various
+        {locked
+      ? <IconLock onClick={triggerUnlock}/>
+      : <IconCloudLockOpen onClick={triggerLock}/>}
+    <p>Using the tabler package, you can add icons to represent various
     different actions and brands</p>
-    <IconBrandPinterest></IconBrandPinterest>
+    <a href="https://pinterest.com"><IconBrandPinterest></IconBrandPinterest></a>
     <IconCheck></IconCheck>
-    <IconX></IconX>
-    <IconLock></IconLock>
+    <a href="https://x.com"><IconBrandTwitter></IconBrandTwitter></a>
     <IconPower></IconPower>
     <IconSettings></IconSettings>
     </div>
   );
 }
-
 export default App;
