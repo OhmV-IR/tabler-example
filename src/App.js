c7432158655094645b157797e90fc415d608ab90
AB <bourdeaux.adrien@gmail.com>
2023-09-26 12:32:06 -0400

    Repositioned elements on the pages

--- src/App.js
+++ src/App.js
@@ -16,19 +16,20 @@ function App() {
   // ? yes condition html goes here
   // : no condition html goes here}
   return (
-    <div className = "leftAlign">
+    <><div className="leftAlign">
+      <p>Using the tabler package, you can add icons to represent various
+        different actions and brands</p>
+      <a href="https://pinterest.com"><IconBrandPinterest className="text-pinterest-red"></IconBrandPinterest></a>
+      <IconX className="text-red"></IconX>
+      <IconCheck className="text-green"></IconCheck>
+      <a href="https://x.com"><IconBrandTwitter className="text-blue"></IconBrandTwitter></a>
+      <IconPower></IconPower>
+      <IconSettings className="text-grey"></IconSettings>
+    </div><div className="topRightCorner">
         {locked
-      ? <IconLock onClick={triggerUnlock}/>
-      : <IconCloudLockOpen onClick={triggerLock}/>}
-    <p>Using the tabler package, you can add icons to represent various
-    different actions and brands</p>
-    <a href="https://pinterest.com"><IconBrandPinterest></IconBrandPinterest></a>
-    <IconX></IconX>
-    <IconCheck></IconCheck>
-    <a href="https://x.com"><IconBrandTwitter></IconBrandTwitter></a>
-    <IconPower></IconPower>
-    <IconSettings></IconSettings>
-    </div>
+          ? <IconLock id="topRightIcon" onClick={triggerUnlock} />
+          : <IconCloudLockOpen id="topRightIcon" onClick={triggerLock} />}
+      </div></>
   );
 }
 export default App;
