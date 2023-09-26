20a73edc1771711c0c4202b38dc598d2990fcb16
AB <bourdeaux.adrien@gmail.com>
2023-09-26 13:04:50 -0400

    added bell icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useState} from 'react';
-import {IconBrandPinterest, IconCloudLockOpen, IconSettings, IconPower, IconLock, IconCheck, IconBrandTwitter, IconX} from '@tabler/icons-react'
+import {IconBellRinging, IconBellRinging2, IconBellRingingFilled, IconBellRinging2Filled, IconBrandPinterest, IconCloudLockOpen, IconSettings, IconPower, IconLock, IconCheck, IconBrandTwitter, IconX} from '@tabler/icons-react'
 function App() {
   // adds a locked variable and creates a function called setLocked
   const [locked, setLocked] = useState(false)
@@ -23,12 +23,18 @@ function App() {
       <IconX className="text-red"></IconX>
       <IconCheck className="text-green"></IconCheck>
       <a href="https://x.com"><IconBrandTwitter className="text-blue"></IconBrandTwitter></a>
-      <IconPower></IconPower>
+      <IconPower className="text-black"></IconPower>
       <IconSettings className="text-grey"></IconSettings>
     </div><div className="topRightCorner">
         {locked
           ? <IconLock id="topRightIcon" onClick={triggerUnlock} />
           : <IconCloudLockOpen id="topRightIcon" onClick={triggerLock} />}
+      </div>
+      <div className="center">
+        <IconBellRinging></IconBellRinging>
+        <IconBellRinging2></IconBellRinging2>
+        <IconBellRingingFilled></IconBellRingingFilled>
+        <IconBellRinging2Filled></IconBellRinging2Filled>
       </div></>
   );
 }
