7a7a11ee8d6d3c2c187c4aea9f24749efbf0835f
AB <bourdeaux.adrien@gmail.com>
2023-10-02 11:45:51 -0400

    organized tabler buttons and added code for cards(not visible yet)

--- src/App.js
+++ src/App.js
@@ -95,13 +95,31 @@ function App() {
         <IconBellRingingFilled></IconBellRingingFilled>
         <IconBellRinging2Filled></IconBellRinging2Filled>
       </div>
-      <div className="right">
-<button className="btn btn-success w-50" onClick={successButtonOnclick}>Successful action(OK)</button>
-<button className="btn btn-warning w-50" onClick={warningButtonOnclick}>Uh oh this is a warning</button>
-<button className="btn btn-danger w-50" onClick={dangerButtonOnclick}>This button starts WW3</button>
-<button className="btn btn-info w-50" onClick={infoButtonOnclick}>Here lies useful information</button>
-      </div></div></>
+<button className="btn btn-success btn-lg button1" onClick={successButtonOnclick}>Successful action(OK) large button</button>
+<button className="btn btn-warning btn-sm w-25 button2" onClick={warningButtonOnclick}>Uh oh this is a warning small button</button>
+<button className="btn btn-danger w-25 button3" onClick={dangerButtonOnclick}>This button starts WW3 25w button</button>
+<button className="btn btn-info w-25 button4" onClick={infoButtonOnclick}>Here lies useful information 25w button</button>
+<button className="btn btn-outline-success w-25 button5">This is an outlined button</button>
+<button className="btn btn-outline-dark w-25 button6">This is a ghost button</button>
+<a href="https://facebook.com" target="_blank" rel="noreferrer"><button className="btn btn-square btn-azure w-25 button7">
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M18 2a1 1 0 0 1 .993 .883l.007 .117v4a1 1 0 0 1 -.883 .993l-.117 .007h-3v1h3a1 1 0 0 1 .991 1.131l-.02 .112l-1 4a1 1 0 0 1 -.858 .75l-.113 .007h-2v6a1 1 0 0 1 -.883 .993l-.117 .007h-4a1 1 0 0 1 -.993 -.883l-.007 -.117v-6h-2a1 1 0 0 1 -.993 -.883l-.007 -.117v-4a1 1 0 0 1 .883 -.993l.117 -.007h2v-1a6 6 0 0 1 5.775 -5.996l.225 -.004h3z" stroke-width="0" fill="currentColor"></path>
+</svg>
+square button Facebook</button></a>
+<button className="btn btn-pill btn-lime button8 w-25">This is a lime pill button</button>
+      <div className="bottom">
+      <div className="card-size-define card">
+  <div className="img-responsive img-responsive card-img-top" id="placeholder-image"></div>
+  <div className="card-body">
+    <h3 className="card-title">Card with title and image</h3>
+    <p className="text-secondary">This is a card</p>
+  </div>
+</div>
+      </div>
+      </div></>
   );
 }
 // Most of the HTML uses predefined css classes that are in app.css and can be found at github.com/tabler/tabler
+// SVGs can be found at tabler-icons.io
 export default App;
